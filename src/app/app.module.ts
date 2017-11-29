3dd1b06ba69bd591799badc6e181ac7e2c92dc82
Duong <duong.dam@contentnet.com.vn>
2017-11-29 11:42:10 +0700

    Fix can't add splaylist component into homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-//import { PlaylistitemComponent } from './shared/components/items/playlistitem/playlistitem.component';
-//import { SplaylistComponent } from "./shared/components/section/splaylist/splaylist.component";
 import { AppRoutingModule } from './app-routing.module';
-//import { VideoitemComponent } from "./shared/components/items/videoitem/videoitem.component";
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    
-    
     
   ],
   imports: [
--- src/app/modules/home-page/home-page.module.ts
+++ src/app/modules/home-page/home-page.module.ts
@@ -5,7 +5,8 @@ import { homeRouter } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
 import { VideoitemComponent } from "../../shared/components/items/videoitem/videoitem.component";
 import { PlaylistitemComponent } from "../../shared/components/items/playlistitem/playlistitem.component";
-//import { SplaylistComponent } from "../../shared/components/section/splaylist/splaylist.component";
+import { SplaylistComponent } from "../../shared/components/section/splaylist/splaylist.component";
+
 @NgModule({
   imports: [
     CommonModule,
@@ -14,7 +15,8 @@ import { PlaylistitemComponent } from "../../shared/components/items/playlistite
   declarations: [
     HomePageComponent,
     VideoitemComponent,
-    PlaylistitemComponent
+    PlaylistitemComponent,
+    SplaylistComponent
     
     
   ]
